488d89b5092666ad5ad95d0a14c4bf2d9de08ca0
kungurtsev <kungurtsev@skbkontur.ru>
2019-05-01 17:31:05 +0500

    implement

---
+++ Vostok.ClusterClient.Topology.SD.Tests/ServiceDiscoveryClusterProvider_Tests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Vostok.Logging.Abstractions;
+using Vostok.Logging.Console;
+using Vostok.ServiceDiscovery.Abstractions;
+using Vostok.ServiceDiscovery.Extensions;
+
+namespace Vostok.ClusterClient.Topology.SD.Tests
+{
+    [TestFixture]
+    internal class ServiceDiscoveryClusterProvider_Tests
+    {
+        private IServiceLocator serviceLocator;
+        private ServiceDiscoveryClusterProvider provider;
+        private readonly ILog log = new SynchronousConsoleLog();
+        private string environment;
+        private string application;
+        private IServiceTopology topology;
+        private IServiceTopologyProperties properties;
+        private Uri[] blacklist;
+
+        private readonly Uri replica1 = new Uri("http://replica1:123/v1/");
+        private readonly Uri replica2 = new Uri("http://replica2:456/v1/");
+
+        [SetUp]
+        public void SetUp()
+        {
+            environment = "environment";
+            application = "application";
+            
+            serviceLocator = Substitute.For<IServiceLocator>();
+            serviceLocator.Locate(environment, application).Returns(_ => topology);
+
+            provider = new ServiceDiscoveryClusterProvider(serviceLocator, environment, application, log);
+
+            blacklist = null;
+
+            properties = Substitute.For<IServiceTopologyProperties>();
+            properties.TryGetValue(IServiceTopologyPropertiesExtensions.BlacklistProperty, out var value)
+                .Returns(
+                    x =>
+                    {
+                        if (blacklist == null)
+                            return false;
+                        x[1] = string.Join(IServiceTopologyPropertiesExtensions.BlacklistItemSeparator, blacklist.Select(xx => xx.ToString()));
+                        return true;
+                    });
+        }
+
+        [Test]
+        public void Should_return_null_for_null_topology()
+        {
+            topology = null;
+            provider.GetCluster().Should().BeNull();
+        }
+
+        [Test]
+        public void Should_return_empty_list_for_empty_replicas()
+        {
+            topology = Substitute.For<IServiceTopology>();
+            topology.Replicas.Returns(new Uri[0]);
+            topology.Properties.Returns(properties);
+
+            provider.GetCluster().Should().BeEmpty();
+        }
+
+        [Test]
+        public void Should_return_replicas()
+        {
+            topology = Substitute.For<IServiceTopology>();
+            topology.Replicas.Returns(new[] {replica1, replica2});
+            topology.Properties.Returns(properties);
+
+            provider.GetCluster().Should().BeEquivalentTo(new[] { replica1, replica2 }.Cast<object>());
+        }
+
+        [Test]
+        public void Should_return_new_replicas_from_new_topology()
+        {
+            topology = Substitute.For<IServiceTopology>();
+            topology.Properties.Returns(properties);
+            topology.Replicas.Returns(new[] { replica1, replica2 });
+            provider.GetCluster().Should().BeEquivalentTo(new[] { replica1, replica2 }.Cast<object>());
+
+            topology = Substitute.For<IServiceTopology>();
+            topology.Properties.Returns(properties);
+            topology.Replicas.Returns(new[] { replica2 });
+            provider.GetCluster().Should().BeEquivalentTo(new[] { replica2 }.Cast<object>());
+        }
+
+        [Test]
+        public void Should_filter_blacklisted_replicas()
+        {
+            topology = Substitute.For<IServiceTopology>();
+            topology.Properties.Returns(properties);
+            topology.Replicas.Returns(new[] { replica1, replica2 });
+
+            blacklist = new[] { replica2 };
+
+            provider.GetCluster().Should().BeEquivalentTo(new[] { replica1 }.Cast<object>());
+        }
+
+        [Test]
+        public void Should_filter_all_blacklisted_replicas()
+        {
+            topology = Substitute.For<IServiceTopology>();
+            topology.Properties.Returns(properties);
+            topology.Replicas.Returns(new[] { replica1, replica2 });
+
+            blacklist = new[] { replica2, replica1 };
+
+            provider.GetCluster().Should().BeEmpty();
+        }
+    }
+}
---
+++ Vostok.ClusterClient.Topology.SD/ServiceDiscoveryClusterProvider.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Vostok.Clusterclient.Core.Topology;
+using Vostok.Commons.Collections;
+using Vostok.Logging.Abstractions;
+using Vostok.ServiceDiscovery.Abstractions;
+using Vostok.ServiceDiscovery.Extensions;
+
+namespace Vostok.ClusterClient.Topology.SD
+{
+    [PublicAPI]
+    public class ServiceDiscoveryClusterProvider : IClusterProvider
+    {
+        private readonly IServiceLocator serviceLocator;
+        private readonly string environment;
+        private readonly string application;
+        private readonly ILog log;
+
+        private readonly CachingTransform<IServiceTopology, Uri[]> transform;
+
+        public ServiceDiscoveryClusterProvider([NotNull] IServiceLocator serviceLocator, [NotNull] string environment, [NotNull] string application, [CanBeNull] ILog log)
+        {
+            this.serviceLocator = serviceLocator;
+            this.environment = environment;
+            this.application = application;
+            this.log = log ?? LogProvider.Get();
+
+            transform = new CachingTransform<IServiceTopology, Uri[]>(ParseReplicas);
+        }
+
+        public IList<Uri> GetCluster()
+            => transform.Get(serviceLocator.Locate(environment, application));
+
+        [CanBeNull]
+        private Uri[] ParseReplicas([CanBeNull] IServiceTopology topology)
+        {
+            if (topology == null)
+            {
+                LogTopologyNotFound();
+                return null;
+            }
+
+            var blacklist = topology.Properties.GetBlacklist();
+            var result = topology.Replicas
+                .Except(blacklist)
+                .ToArray();
+
+            LogResolvedReplicas(result);
+
+            return result;
+        }
+
+        #region Logging
+
+        private void LogTopologyNotFound()
+        {
+            log.Warn("Topology of '{Application} application in '{Environment}' environment was not found in ServiceDiscovery.", application, environment);
+        }
+
+        private void LogResolvedReplicas(Uri[] replicas)
+        {
+            if (replicas.Length == 0)
+            {
+                log.Info("Resolved ServiceDiscovery topology of '{Application} application in '{Environment}' to an empty set of replicas.", application, environment);
+            }
+            else
+            {
+                log.Info("Resolved ServiceDiscovery topology of '{Application} application in '{Environment}' to following replicas: \n\t{Replicas}",
+                    application, environment, string.Join("\n\t", replicas as IEnumerable<Uri>));
+            }
+        }
+
+        #endregion
+    }
+}

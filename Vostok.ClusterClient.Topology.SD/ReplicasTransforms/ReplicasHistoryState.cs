db535c2deb4972d3d0818b5fe321e984e4217cf5
deniaa <deniaa@skbkontur.ru>
2021-04-23 23:28:02 +0500

    Revert "Remove TransformWithHistory.cs to release only simple strategies first."
    This reverts commit 044bf76938a1035a94a5d15df0d21f15707aebb1.
    
    Just to continue research for advanced caches.

---
+++ Vostok.ClusterClient.Topology.SD/ReplicasTransforms/ReplicasHistoryState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Vostok.Clusterclient.Topology.SD.ReplicasTransforms
+{
+    public class ReplicasHistoryState
+    {
+        public ReplicasHistoryState(int maxObservedAliveReplicas, Uri[] buffer, int aliveCount, int staleCount)
+        {
+            MaxObservedAliveReplicas = maxObservedAliveReplicas;
+            this.aliveCount = aliveCount;
+            
+            AliveAndStaleReplicas = new ArraySegment<Uri>(buffer, 0, aliveCount + staleCount);
+            aliveReplicas = new ArraySegment<Uri>(buffer, 0, aliveCount);
+        }
+
+        public readonly int MaxObservedAliveReplicas;
+        public ArraySegment<Uri> AliveAndStaleReplicas;
+        public ArraySegment<Uri> aliveReplicas;
+        public readonly int aliveCount;
+    }
+}

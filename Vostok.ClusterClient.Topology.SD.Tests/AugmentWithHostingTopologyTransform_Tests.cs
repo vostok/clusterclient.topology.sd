8160ce0d9841d68345397b0ebb90616a15f43483
iloktionov <iloktionov@skbkontur.ru>
2022-05-16 20:24:17 +0500

    + AugmentWithHostingTopologyTransform

---
+++ Vostok.ClusterClient.Topology.SD.Tests/AugmentWithHostingTopologyTransform_Tests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Vostok.Clusterclient.Topology.SD.Transforms;
+using Vostok.Logging.Abstractions;
+using Vostok.Logging.Console;
+using Vostok.ServiceDiscovery.Abstractions;
+using Vostok.ServiceDiscovery.Abstractions.Models;
+using Vostok.ServiceDiscovery.Extensions;
+
+namespace Vostok.Clusterclient.Topology.SD.Tests;
+
+[TestFixture]
+internal class AugmentWithHostingTopologyTransform_Tests
+{
+    private readonly ILog log = new SynchronousConsoleLog();
+
+    private readonly Uri replica1 = new Uri("http://replica1:123/v1/");
+    private readonly Uri replica2 = new Uri("http://replica2:456/v1/");
+    private readonly Uri replica3 = new Uri("http://replica3:789/");
+    private readonly Uri replica4 = new Uri("http://replica4.dev.kontur.ru:111/");
+    private readonly Uri replica5 = new Uri("http://replica5.dev.kontur.ru:222/");
+    private readonly Uri replica6 = new Uri("http://replica6.dev.kontur.ru:333/");
+
+    private IServiceLocator serviceLocator;
+    private ServiceDiscoveryClusterProvider provider;
+    private string environment;
+    private string application;
+
+    private Uri[] beacons;
+    private Uri[] hosting;
+
+    [SetUp]
+    public void SetUp()
+    {
+        environment = "environment";
+        application = "application";
+
+        beacons = null;
+        hosting = null;
+
+        serviceLocator = Substitute.For<IServiceLocator>();
+        serviceLocator.Locate(environment, application).Returns(_ => BuildTopology());
+
+        var settings = new ServiceDiscoveryClusterProviderSettings
+        {
+            ServiceTopologyTransform = new AugmentWithHostingTopologyTransform()
+        };
+
+        provider = new ServiceDiscoveryClusterProvider(serviceLocator, environment, application, settings, log);
+    }
+
+    [Test]
+    public void Should_return_beacons_when_there_is_no_hosting_topology()
+    {
+        SetupBeacons(replica1, replica2);
+
+        provider.GetCluster().Should().Equal(replica1, replica2);
+    }
+
+    [Test]
+    public void Should_return_empty_when_there_is_no_hosting_topology()
+    {
+        SetupBeacons();
+
+        provider.GetCluster().Should().BeEmpty();
+    }
+
+    [Test]
+    public void Should_return_beacons_when_topologies_match()
+    {
+        SetupBeacons(replica1, replica2, replica3);
+
+        SetupHosting(replica3, replica2, replica1);
+
+        provider.GetCluster().Should().Equal(replica1, replica2, replica3);
+    }
+    
+    [Test]
+    public void Should_return_beacons_when_there_is_still_enough_of_them()
+    {
+        SetupBeacons(replica1, replica2);
+
+        SetupHosting(replica3, replica2, replica1);
+
+        provider.GetCluster().Should().Equal(replica1, replica2);
+    }
+
+    [Test]
+    public void Should_return_beacons_when_there_are_not_enough_of_them_but_topologies_diverge()
+    {
+        SetupBeacons(replica1);
+
+        SetupHosting(replica5, replica6, replica4);
+
+        provider.GetCluster().Should().Equal(replica1);
+    }
+
+    [Test]
+    public void Should_merge_when_there_are_no_beacons()
+    {
+        SetupBeacons();
+
+        SetupHosting(replica1, replica2, replica3);
+
+        provider.GetCluster().Should().BeEquivalentTo(replica1, replica2, replica3);
+    }
+
+    [Test]
+    public void Should_merge_when_there_are_not_enough_beacons()
+    {
+        SetupBeacons(replica2);
+
+        SetupHosting(replica3, replica2, replica1);
+
+        provider.GetCluster().Should().BeEquivalentTo(replica1, replica2, replica3);
+    }
+
+    [Test]
+    public void Should_prefer_beacon_url_when_merging_common_replicas()
+    {
+        SetupBeacons(replica2);
+
+        SetupHosting(replica3, ScrambleUri(replica2), replica1);
+
+        provider.GetCluster().Should().BeEquivalentTo(replica1, replica2, replica3);
+    }
+
+    private void SetupBeacons(params Uri[] replicas)
+        => beacons = replicas;
+
+    private void SetupHosting(params Uri[] replicas)
+        => hosting = replicas;
+
+    private IServiceTopology BuildTopology()
+    {
+        if (hosting == null)
+            return ServiceTopology.Build(beacons, null);
+
+        var properties = new ApplicationInfo(environment, application, null).Properties.SetHostingTopology(hosting);
+
+        return ServiceTopology.Build(beacons, properties);
+    }
+
+    private Uri ScrambleUri(Uri uri)
+    {
+        var builder = new UriBuilder(uri)
+        {
+            Path = string.Empty
+        };
+
+        builder.Host += ".domain";
+
+        return builder.Uri;
+    }
+}
---
+++ Vostok.ClusterClient.Topology.SD/Transforms/AugmentWithHostingTopologyTransform.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Vostok.Commons.Helpers.Topology;
+using Vostok.ServiceDiscovery.Abstractions;
+using Vostok.ServiceDiscovery.Extensions;
+
+namespace Vostok.Clusterclient.Topology.SD.Transforms;
+
+/// <summary>
+/// A transform that returns the union of resolved replicas and replicas specified by the app's hosting system in a dedicated property
+/// when it detects that resolved topology is considerably smaller than it should be (see <see cref="AcceptableAliveBeaconsRatio"/>).
+/// </summary>
+[PublicAPI]
+public class AugmentWithHostingTopologyTransform : IServiceTopologyTransform
+{
+    /// <summary>
+    /// Resolved topology that is at least as large as this fraction of the corresponding hosting topology won't be transformed.
+    /// </summary>
+    public double AcceptableAliveBeaconsRatio { get; set; } = 2 / 3d;
+
+    /// <summary>
+    /// Resolved topology's fraction of replicas that are also in hosting topology must be no less than this value in order for transformation to occur.
+    /// </summary>
+    public double MinCommonReplicasRatio { get; set; } = 2 / 3d;
+
+    public IEnumerable<Uri> Transform(IServiceTopology topology)
+    {
+        var hostingTopology = GetHostingTopology(topology);
+
+        if (!ShouldMerge(topology.Replicas, hostingTopology))
+            return topology.Replicas;
+
+        // (iloktionov, 16.05.2022): Preserve exact addresses from beacons when there are differences (such as URL path or domain name) ignored by ReplicaComparer:
+        foreach (var replica in topology.Replicas)
+            hostingTopology[replica] = replica;
+
+        return hostingTopology.Values;
+    }
+
+    private bool ShouldMerge(IReadOnlyList<Uri> beacons, Dictionary<Uri, Uri> hostingTopology)
+    {
+        if (hostingTopology.Count == 0)
+            return false;
+
+        if (beacons.Count == 0)
+            return true;
+
+        if (beacons.Count >= hostingTopology.Count * AcceptableAliveBeaconsRatio)
+            return false;
+
+        var commonReplicasCount = beacons.Count(hostingTopology.ContainsKey);
+        var commonReplicasRatio = 1d * commonReplicasCount / beacons.Count;
+        if (commonReplicasRatio < MinCommonReplicasRatio)
+            return false;
+
+        return true;
+    }
+
+    private static Dictionary<Uri, Uri> GetHostingTopology(IServiceTopology topology)
+    {
+        var replicas = topology.Properties.GetHostingTopology();
+
+        var result = new Dictionary<Uri, Uri>(replicas.Length, ReplicaComparer.Instance);
+
+        foreach (var replica in replicas)
+            result[replica] = replica;
+
+        return result;
+    }
+}
